1de57c090d29546cf949d67549af69db07de3246
IritMaximmus <aaro.yliayho@gmail.com>
2021-05-13 17:05:46 +0300

    b osan välitehtävät

--- osa1/osa1_palautus/src/App.js
+++ osa1/osa1_palautus/src/App.js
@@ -1,56 +1,66 @@
 import React from 'react'
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} ex1={exercises1} ex2={exercises2}
-       ex3={exercises3} />
-      <Total excs={exercises1 + exercises2 + exercises3} />
+       <Header course={course.name}/>
+       <Content parts={course.parts} />
+       <Total parts={course.parts} />
     </div>
   )
 }
 
 const Header = (props) => {
-  return(
-    <div>
-      <h1> {props.course}</h1>
-    </div>
-  )
+  console.log(props)
+  return <h1> {props.course}</h1>
 }
 
 const Content = (props) => {
+
+  const first = props.parts[0]
+  const second = props.parts[1]
+  const third = props.parts[2]
+
+
   return (
-    <div>
-      <Part part={props.part1} exc={props.ex1} />
-      <Part part={props.part2} exc={props.ex2} />
-      <Part part={props.part3} exc={props.ex3} />
-    </div>
+    <>
+      <Part part={first}/>
+      <Part part={second}/>
+      <Part part={third}/>
+    </>
   )
 }
 
 const Part = (props) => {
-  return (
-    <div>
-      <p>{props.part} {props.exc}</p>
-    </div>
-  )
+  console.log(props)
+  return <p>{props.part.name} {props.part.exercises}</p>
 }
 
 const Total = (props) => {
-  return (
-    <div>
-      <p>Number of excercises {props.excs}</p>
-    </div>
-  )
+  let exercises = 0
+
+  props.parts.forEach(values => {
+    exercises += values.exercises
+  })
+  return <p>Number of exercises {exercises}</p>
 }
 
 export default App
